fe5f5683b78e9683023aa9a262be25cee5484b23
Bruno Erceg <brunoerceg95@gmail.com>
2026-01-11 15:23:19 +0100

    refactor: implement CVA for Stack component styling

--- components/layout/Stack.tsx
+++ components/layout/Stack.tsx
@@ -1,29 +1,37 @@
-import { cn } from "@utils/cn";
+import { cn } from '@utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import { HTMLAttributes } from 'react';
 
-type StackProps = {
-  children: React.ReactNode;
-  className?: string;
-  direction?: "col" | "row";
-  gap?: "none" | "sm" | "md" | "lg";
-};
+type StackProps = VariantProps<typeof stackVariants> & HTMLAttributes<HTMLDivElement>;
 
-function Stack({
-  direction = "row",
-  gap = "md",
-  className,
-  children,
-}: StackProps) {
-  const getGapClass: any = () => {
-    const size = {
-      none: "",
-      sm: "gap-2",
-      md: "gap-4",
-      lg: "gap-8",
-    };
-    return gap ? size[gap] : "";
-  };
+const stackVariants = cva('flex', {
+  variants: {
+    gap: {
+      none: '',
+      sm: 'gap-2',
+      md: 'gap-4',
+      lg: 'gap-8',
+    },
+    direction: {
+      col: 'flex-col',
+      row: 'flex-row items-center',
+    },
+  },
+  defaultVariants: {
+    gap: 'md',
+    direction: 'row',
+  },
+});
+
+/**
+ * A React component that displays a stack of elements with a variant style.
+ * The component is used to layout pages and sections.
+ *
+ * @returns - The JSX element of the component.
+ */
+function Stack({ direction, gap, className, children, ...props }: StackProps) {
   return (
-    <div className={cn(`flex flex-${direction} ${getGapClass()}`, className)}>
+    <div className={cn(stackVariants({ gap, direction }), className)} {...props}>
       {children}
     </div>
   );
